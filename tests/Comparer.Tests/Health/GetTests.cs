6bdfc43f84c5bb2beeea266a24f7fc66e3cb0e49
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-05-13 08:50:09 +0100

    CDMS-580: exposes health authorized endpoint for use in external components health checks. (#28)

--- tests/Comparer.Tests/Health/GetTests.cs
+++ tests/Comparer.Tests/Health/GetTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Defra.TradeImportsDecisionComparer.Comparer.Tests.Endpoints;
 using Xunit.Abstractions;
 
@@ -16,5 +17,25 @@ public class GetTests(ComparerWebApplicationFactory factory, ITestOutputHelper o
         await Verify(response);
         await Verify(await response.Content.ReadAsStringAsync())
             .UseMethodName(nameof(Get_Health_ReturnsAnonymous) + "_content");
+    }
+
+    [Fact]
+    public async Task Get_HealthAuthorized_ReturnsUnauthorized()
+    {
+        var client = CreateClient(addDefaultAuthorizationHeader: false);
+
+        var response = await client.GetAsync("/health/authorized");
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Get_HealthAuthorized_WhenAuthorized_ReturnsOk()
+    {
+        var client = CreateClient();
+
+        var response = await client.GetAsync("/health/authorized");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
