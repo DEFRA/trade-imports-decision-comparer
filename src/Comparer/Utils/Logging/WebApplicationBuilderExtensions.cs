b1e97a9276f9aa561494f0a49abec1632279cf5f
Tim Peel <tim.peel@equalexperts.com>
2025-06-11 10:17:18 +0100

    Silence volume of logs related to health endpoint calls

--- src/Comparer/Utils/Logging/WebApplicationBuilderExtensions.cs
+++ src/Comparer/Utils/Logging/WebApplicationBuilderExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HeaderPropagation;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using Serilog;
+using Serilog.Events;
 
 namespace Defra.TradeImportsDecisionComparer.Comparer.Utils.Logging;
 
@@ -59,7 +60,13 @@ public static class WebApplicationBuilderExtensions
             .Enrich.WithEcsHttpContext(httpAccessor)
             .Enrich.FromLogContext()
             .Enrich.With(new TraceContextEnricher())
-            .Enrich.WithCorrelationId(traceHeader.Name);
+            .Enrich.WithCorrelationId(traceHeader.Name)
+            .Filter.ByExcluding(x =>
+                x.Level == LogEventLevel.Information
+                && x.Properties.TryGetValue("RequestPath", out var path)
+                && path.ToString().Contains("/health")
+                && !x.MessageTemplate.Text.StartsWith("Request finished")
+            );
 
         if (!string.IsNullOrWhiteSpace(serviceVersion))
             config.Enrich.WithProperty("service.version", serviceVersion);
