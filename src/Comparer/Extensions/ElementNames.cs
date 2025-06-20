7f5b70e0dfb94b81a59b232d94e82a1bbd3160ea
Thomas Anderson <t11omas1983@live.co.uk>
2025-06-20 12:49:27 +0100

    Ordered decision by decision number so it compares the highest decision number

--- src/Comparer/Extensions/ElementNames.cs
+++ src/Comparer/Extensions/ElementNames.cs
@@ -9,6 +9,8 @@ public static class ElementNames
     public static readonly XName Item = XName.Get(nameof(Item), Namespace);
     public static readonly XName Check = XName.Get(nameof(Check), Namespace);
     public static readonly XName ItemNumber = XName.Get(nameof(ItemNumber), Namespace);
+    public static readonly XName Header = XName.Get(nameof(Header), Namespace);
+    public static readonly XName DecisionNumber = XName.Get(nameof(DecisionNumber), Namespace);
     public static readonly XName DecisionCode = XName.Get(nameof(DecisionCode), Namespace);
     public static readonly XName CheckCode = XName.Get(nameof(CheckCode), Namespace);
     public static readonly XName DecisionValidUntil = XName.Get(nameof(DecisionValidUntil), Namespace);
